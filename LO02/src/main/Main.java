ba2afdd6d74ade29b29ef687b596de4574045fa2
YomeR7 <33492316+YomeR7@users.noreply.github.com>
2017-11-16 10:40:04 +0100

    new main out print

--- LO02/src/main/Main.java
+++ LO02/src/main/Main.java
@@ -9,7 +9,7 @@ public final class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("JEU DU 8 AMERICAIN\nRéalisé par Florian Esslinger et Guillaume Rousselet\n");
+		System.out.println("JEU DU 8 AMERICAIN\nRéalisé par Florian Esslinger et Guillaume Rousselet Lateub\n");
 		Partie maPartie = new Partie();
 		
 	}	
