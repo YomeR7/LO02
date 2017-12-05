425728188fe2b8b3fd217a8e3ee12899b39f17fa
YomeR7 <33492316+YomeR7@users.noreply.github.com>
2017-12-05 23:55:52 +0100

    man et rasta

--- LO02/src/main/VarianteSimple.java
+++ LO02/src/main/VarianteSimple.java
@@ -4,7 +4,11 @@ public class VarianteSimple extends Variante{
 
 	public VarianteSimple() {
 		super();
-		this.nom = "simple";
-		valeurEffetAttaque.put("5", new EffetPiocher2());
+		this.nom = "Man et Resta simplifié";
+		valeurEffetAttaque.put("2", new EffetPiocher2());
+		valeurEffetDefense.put("10", new EffetRejouer());
+		valeurEffetAttaque.put("7", new EffetPasseTour());
+		valeurEffetAttaque.put("As", new EffetPiocher2ouContre());
+		valeurEffetDefense.put("Valet", new EffetPiocher2());
 	}
 }
